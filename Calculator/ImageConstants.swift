7bd09486c7f396b7b1ba60d645f08cbd8b3bea4c
Charlesbsc92 <charlesbsc92@gmail.com>
2026-08-26 11:10:25 +0530

    [Feature]Color Constants added

---
+++ Calculator/ImageConstants.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  Calculator
+//
+//  Created by Charles on 26/08/26.
+//
+
+import SwiftUI
+
+let buttonBackgroundColor:Color = Color("buttonBackground")
+let foregroundDigitsColor:Color = Color("foregroundDigits")
+let foregroundRightButtonColor:Color = Color("foregroundRightButtonColor")
+let foregroundTopButtonColor:Color = Color("foregroundTopButtonColor")
+let primaryBackgroundColor:Color = Color("primaryBackground")
+let secondaryBackgroundColor:Color = Color("SecondaryBackground")
